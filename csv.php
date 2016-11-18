893a7f7c8112ef3994d0ff213380bdb6f9ada11a
stas <potapenko.stanislav@gmail.com>
2016-11-18 12:16:57 +0300

    FIxed descr end line for lightings

--- csv.php
+++ csv.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/config.php';
 require_once 'functions.php';
 
 //$url = 'http://localhost/test/msc/lightning.html';
-$url = 'lightning.html';
+$url = 'vanities-toilets.html';
 //$url = 'kitchens-finishes.html';
 $page =  @file_get_contents(SITE_URL . $url);
 //get heads
@@ -22,11 +22,11 @@ $items = get_data_from_html($xpath);
 
 $products = map_products_from_data($items);
 
-$csv_data = prepare_csv_data($products, 'lighting');
+$csv_data = prepare_csv_data($products, 'toilets');
 
 var_dump($csv_data);
 
-$filename = 'lighting.csv';
+$filename = 'toilets.csv';
 
 write_csv_file($filename, $csv_data);
 //generate csv
